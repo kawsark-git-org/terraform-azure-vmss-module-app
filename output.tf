ce4616f938f7fc9176d99f83b224b50ea08da2be
kawsark <kawsar@hashicorp.com>
2018-08-30 02:11:32 -0400

    Initial commit for module

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "redis_cache" {
+  value = "${azurerm_redis_cache.vmss_redis.hostname}:${azurerm_redis_cache.vmss_redis.port}"
+}
+
+output "app_public_ip" {
+    value = "${azurerm_public_ip.app-public-ip.fqdn}"
+}
